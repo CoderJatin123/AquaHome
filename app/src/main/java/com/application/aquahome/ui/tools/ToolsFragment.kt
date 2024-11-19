1ce4f01e05219b218f876adf504ee9f4c64c2470
ProfessionalJatin <professionaljatin101@gmail.com>
2024-11-19 22:01:44 +0530

    Add time slot functionality started development.

--- app/src/main/java/com/application/aquahome/ui/tools/ToolsFragment.kt
+++ app/src/main/java/com/application/aquahome/ui/tools/ToolsFragment.kt
@@ -1,10 +1,13 @@
 package com.application.aquahome.ui.tools
 
+import android.app.TimePickerDialog
+import android.icu.util.Calendar
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.application.aquahome.databinding.FragmentToolsBinding
@@ -12,9 +15,6 @@ import com.application.aquahome.databinding.FragmentToolsBinding
 class ToolsFragment : Fragment() {
 
     private var _binding: FragmentToolsBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -22,21 +22,35 @@ class ToolsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+
+//        val notificationsViewModel =
+//            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+        val c = Calendar.getInstance()
+        val hour = c.get(Calendar.HOUR_OF_DAY)
+        val minute = c.get(Calendar.MINUTE)
 
         _binding = FragmentToolsBinding.inflate(inflater, container, false)
         val root: View = binding.root
+        val timePickerDialog = TimePickerDialog(context,
+            { view, h, m ->
+                Toast.makeText(context, "$h $m", Toast.LENGTH_SHORT).show()
 
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+            },hour,minute,false)
+
+        binding.addTimeBtn.setOnClickListener {
+            timePickerDialog.show()
         }
+
+
+
         return root
+
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
+
 }
